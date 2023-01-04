f8375ffec78a3fa4d626c74b77d911a79a2fd959
AL424 <algr5113@gmail.com>
2023-01-04 19:49:50 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+  let i = 0;
+  let resultStr = '';
+
+  let nStr = String(n);
+
+  if (nStr[0] === '-') nStr = nStr.slice(1);
+
+  while (i < nStr.length) {
+    resultStr = `${nStr[i]}${resultStr}`;
+    i += 1;
+  }
+
+  let resultNum = Number(resultStr);
+
+  return resultNum;
 }
